fbacf1e389f76937e2a52d4151e6061aca50210d
ryanp <ryanpanimation@gmail.com>
2020-09-30 23:42:20 -0500

    selection sort test OK

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,17 +1,18 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(0, len(arr)):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
-        # Your code here
-
-
+        # ! Your code here
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
         # TO-DO: swap
-        # Your code here
-
+        # ! Your code here
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
     return arr
 
 
@@ -19,10 +20,10 @@ def selection_sort(arr):
 def bubble_sort(arr):
     # Your code here
 
-
     return arr
 
-'''
+
+"""
 STRETCH: implement the Counting Sort function below
 
 Counting sort is a sorting algorithm that works on a set of data where
@@ -38,9 +39,10 @@ showed up, we can construct a sorted set of the input data from the
 buckets. 
 
 What is the time and space complexity of the counting sort algorithm?
-'''
+"""
+
+
 def counting_sort(arr, maximum=None):
     # Your code here
-
 
     return arr
