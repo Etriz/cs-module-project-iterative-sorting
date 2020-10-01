8a7779a97734fee638cb0e0a72cf753873d62743
ryanp <ryanpanimation@gmail.com>
2020-10-01 01:42:31 -0500

    bubble sort tests OK

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -18,10 +18,50 @@ def selection_sort(arr):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
+    #! Your code here
+    # looping over whole array
+    for i in range(len(arr)):
+        # looping over whole array again
+        for j in range(len(arr) - 1):
+            # compare numbers
+            if arr[j] > arr[i]:
+                # swap the two numbers if needed
+                arr[j], arr[i] = arr[i], arr[j]
 
+        # if i < len(arr) - 1 and arr[i] > arr[i + 1]:
+        #     arr[i], arr[i + 1] = arr[i + 1], arr[i]
     return arr
 
+
+"""
+def insertion_sort(list_to_sort):
+    # seperate the first element and think of it as sorted
+    # let us say it is the first item in the list
+
+    # for all other items (on the right of the first index)
+    # range based starting at index 1
+    for i in range(1, len(list_to_sort)):
+        # put the current number in to a temporary variable (temp)
+        temp = list_to_sort[i]
+
+        # keep track of our current index as j
+        j = i
+
+        # keep looking left, until we find where our temp number belongs
+        # while j is greater than zero (we are not past the start of the indces)
+        # and our temp variable is less than the number to the left of j
+        while j > 0 and temp < list_to_sort[j - 1]:
+            # as we look left we can shift the items to the right as we iterate
+            list_to_sort[j] = list_to_sort[j - 1]
+            # decrement j
+            j -= 1
+
+        # when left is smaller than temp, or we are at zero, put the item at that spot
+        list_to_sort[j] = temp
+
+    # return the list back to the caller
+    return list_to_sort
+"""
 
 """
 STRETCH: implement the Counting Sort function below
